d782cdcb1b9222c6d89f4108d6bd093d367c0a38
Adxell <adxell02@gmail.com>
2021-11-22 21:56:22 -0500

    update urlpatterns

--- webfinal/webfinal/urls.py
+++ webfinal/webfinal/urls.py
@@ -1,30 +1,17 @@
-"""webfinal URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
-from personas.views import index, adminS
-
+from django.conf.urls import url, include
+from personas.views import index, registro, mostrar, delatepersona, editpersona, PDF, PDFtodos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index),
-    path('adminS/', adminS),
-    # path('login/', login),
+    path('registro/', registro),
+    path('registros/', mostrar),
+    path('registros/', mostrar),
+    path('registros/<id>', delatepersona),
+    path('editar/<id>', editpersona),
+    path('indexPDF/PDF/<id>', PDF),
+    path('indexPDF/PDFtodos/', PDFtodos),
     path('account/', include('django.contrib.auth.urls')),
     url(r'^$', index),
 ]
